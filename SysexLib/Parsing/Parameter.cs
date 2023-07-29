8a2fd787e490b9d2591bd5ca526d3e660f702a67
Lars Ahlzen <lars@ahlzen.com>
2023-07-29 14:52:38 -0400

    Further work on parsing of sysex parameters.

--- SysexLib/Parsing/Parameter.cs
+++ SysexLib/Parsing/Parameter.cs
@@ -1,6 +1,23 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Text;
 
 namespace Ahlzen.SysexSharp.SysexLib.Parsing;
+
+/// <summary>
+/// Contains information about why a parameter value is not
+/// valid and, if possible, a corrected value that is within the
+/// valid range for this parameter.
+/// </summary>
+public class ValidationException<T> : Exception
+{
+    public T CorrectedValue { get; }
+    public ValidationException(string message, T correctedValue) :
+        base(message)
+    {
+        CorrectedValue = correctedValue;
+    }
+}
 
 public abstract class Parameter<T>
 {
@@ -8,7 +25,7 @@ public abstract class Parameter<T>
     /// Validates that the specified value is valid
     /// for this parameter (length, range, etc)
     /// </summary>
-    /// <exception cref="ArgumentException">
+    /// <exception cref="ValidationException">
     /// Thrown if the specified value is not within the allowable
     /// range.
     /// </exception>
@@ -18,24 +35,27 @@ public abstract class Parameter<T>
     /// Returns this parameter's current value in the
     /// specified data. Does not validate the value.
     /// </summary>
-    /// <exception cref="InvalidOperationException">
+    /// <exception cref="ArgumentException">
     /// Thrown if the value cannot be determined, for example
     /// if the specified data is invalid or too short.
     /// </exception>
     /// <param name="data">The full sysex data.</param>
-    /// <returns></returns>
     public abstract T GetValue(byte[] data);
 
     /// <summary>
     /// Updates this parameter's current value in the
     /// specified data. Does not validate the value.
     /// </summary>
+    /// <exception cref="ArgumentException">
+    /// Thrown if the value cannot be set as specified, for example
+    /// if the parameter extends beyond the length of the data.
+    /// </exception>
     /// <param name="data">The full sysex data.</param>
     public abstract void SetValue(byte[] data, T value);
 }
 
 /// <summary>
-/// ASCII string parameter. Space (0x20) padded.
+/// ASCII string parameter. Space (0x20) padded in data.
 /// </summary>
 public class AsciiParameter : Parameter<string>
 {
@@ -55,22 +75,33 @@ public class AsciiParameter : Parameter<string>
     public override void Validate(string value)
     {
         if (value == null)
-            throw new ArgumentNullException(nameof(value));
+            throw new ValidationException<string>("Value is null.", "");
         if (value.Length > this.Length)
-            throw new ArgumentException(
-                "String is too long.", nameof(value));
-        // TODO: Check for non-ascii and non-printable characters
-
+            throw new ValidationException<string>(
+                "String is too long.", value.Substring(0,Length));
+        if (ParsingUtils.HasNonPrintableCharacters(value))
+            throw new ValidationException<string>(
+                "String contains non-printable characters.",
+                ParsingUtils.ReplaceNonPrintableCharacters(value));
     }
 
     public override string GetValue(byte[] data)
     {
-        throw new NotImplementedException();
+        if (Offset + Length > data.Length)
+            throw new ArgumentException(
+                "String value extends beyond end of data.", nameof(data));
+        return Encoding.ASCII.GetString(data, Offset, Length).TrimEnd();
     }
 
     public override void SetValue(byte[] data, string value)
     {
-        throw new NotImplementedException();
+        if (Offset + value.Length > data.Length)
+            throw new ArgumentException(
+                "String value extends beyond end of data.", nameof(value));
+        if (value.Length > this.Length)
+            throw new ArgumentException("String is too long.");
+        Array.Fill<byte>(data, 0x20, Offset, Length); // space-pad
+        Encoding.ASCII.GetBytes(value, 0, value.Length, data, Offset);
     }
 }
 
@@ -100,6 +131,12 @@ public class NumericParameter : Parameter<int>
         int minValue = 0, int maxValue = 127,
         int bitCount = 7, int bitOffset = 0)
     {
+        Debug.Assert(name != null);
+        Debug.Assert(offset >= 0);
+        Debug.Assert(maxValue >= minValue);
+        Debug.Assert(bitCount > 0);
+        Debug.Assert(bitOffset >= 0);
+
         Name = name;
         Offset = offset;
         MinValue = minValue;
@@ -110,17 +147,36 @@ public class NumericParameter : Parameter<int>
 
     public override void Validate(int value)
     {
-        throw new NotImplementedException();
+        if (value < MinValue)
+            throw new ValidationException<int>(
+                "Value is too small.", MinValue);
+        if (value > MaxValue)
+            throw new ValidationException<int>(
+                "Value is too large.", MaxValue);
     }
 
     public override int GetValue(byte[] data)
     {
-        throw new NotImplementedException();
+        if (Offset >= data.Length)
+            throw new ArgumentException(
+                "Data is not long enough");
+
+        int mask = (1 << BitCount) - 1;
+        int value = ((int)data[Offset] >> BitOffset) & mask;
+        return value;
     }
 
     public override void SetValue(byte[] data, int value)
     {
-        throw new NotImplementedException();
+        if (Offset >= data.Length)
+            throw new ArgumentException(
+                "Data is not long enough");
+
+        int srcMask = (1 << BitCount) - 1;
+        int dstMask = 0xff - (srcMask << BitOffset);
+        value = (value << BitOffset) & srcMask;
+        data[Offset] &= (byte)dstMask; // clear parameter's bit in byte
+        data[Offset] |= (byte)value; // set parameter value
     }
 }
 
