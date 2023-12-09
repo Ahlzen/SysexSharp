d43a1c98acd66231f5ea16c260e8c2fe0b0c99e0
Lars Ahlzen <lars@ahlzen.com>
2023-12-08 21:59:28 -0500

    Cleaned up obsolete code.

--- SysexLib/SysexFactory.cs
+++ SysexLib/SysexFactory.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Runtime.CompilerServices;
-using Ahlzen.SysexSharp.SysexLib.Utils;
+﻿using System.IO;
 using Ahlzen.SysexSharp.SysexLib.Manufacturers.MIDI;
 using Ahlzen.SysexSharp.SysexLib.Manufacturers.Roland;
 using Ahlzen.SysexSharp.SysexLib.Manufacturers.Yamaha;
-using System.Linq;
 
 namespace Ahlzen.SysexSharp.SysexLib;
 
@@ -48,57 +43,5 @@ public static class SysexFactory
             default:
                 return new Sysex(data, filename);
         }
-
-
-        //List<int> offsets = CompositeSysex.GetSysexOffsets(data);
-        //if (offsets.Count == 1)
-        //{
-        //    // Single sysex
-        //    return CreateSingleSysex(data, filename);
-        //}
-        //else
-        //{
-        //    // Composite (multi) sysex
-        //    return CreateCompositeSysex(data, offsets, filename);
-        //}
     }
-
-    //private static Sysex CreateSingleSysex(byte[] data, string? filename)
-    //{
-    //    byte[] manufacturerId = Manufacturers.Manufacturers.GetId(data);
-    //    string? manufacturerName = Manufacturers.Manufacturers.GetName(manufacturerId);
-
-    //    // Special case: Universal messages have no Manufacturer
-
-    //    if (manufacturerId[0] == 0x7e // non-realtime universal message
-    //        || manufacturerId[0] == 0x7f) // realtime unversal message
-    //    {
-    //        return new UniversalSysex(data);
-    //    }
-
-    //    switch (manufacturerName)
-    //    {
-    //        case "Roland":
-    //            return new RolandSysex(data, filename);
-    //        case "Yamaha":
-    //            return YamahaFactory.Create(data, filename);
-    //        case "Korg":
-    //            return new Sysex(data, filename); // TODO
-
-    //        // Add additional manufacturers with detailed support here
-
-    //        default:
-    //            return new Sysex(data, filename);
-    //    }
-    //}
-
-    //private static CompositeSysex CreateCompositeSysex(
-    //    byte[] data, List<int> offsets, string? filename)
-    //{
-    //    var compositeSysex = new CompositeSysex(data);
-
-        
-
-    //    return compositeSysex;
-    //}
 }
