5ed26a3d429ff73ba5da8d7ca6357e4af1d775e5
Lars Ahlzen <lars@ahlzen.com>
2023-12-27 06:18:13 -0500

    Implemented ICanParse for TX81ZVoice.

--- SysexLib/Manufacturers/Yamaha/TX81ZVoice.cs
+++ SysexLib/Manufacturers/Yamaha/TX81ZVoice.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
 using Ahlzen.SysexSharp.SysexLib.Parsing;
 using Ahlzen.SysexSharp.SysexLib.Utils;
 
 namespace Ahlzen.SysexSharp.SysexLib.Manufacturers.Yamaha
 {
-    public class TX81ZVoice : CompositeSysex
+    public class TX81ZVoice : CompositeSysex, ICanParse
     {
         public TX81ZVoice(byte[] data) : base(data)
         {
@@ -26,6 +28,9 @@ namespace Ahlzen.SysexSharp.SysexLib.Manufacturers.Yamaha
         public override string? Device => "TX81Z";
         public override string? Type => "Single voice";
         public override string? Name => GetItem(1).Name;
+
+        private const int DX21VoiceOffset = DXData.TX81ZAdditionalVoiceDataTotalLength;
+        private const int TX81ZAdditionalDataOffset = 0;
 
         public new static bool Test(byte[] data)
         {
@@ -65,5 +70,44 @@ namespace Ahlzen.SysexSharp.SysexLib.Manufacturers.Yamaha
             return tx81ZAdditionalData.GetData()
                 .Append(dx21Voice.GetData());
         }
+
+        #region ICanParse
+
+        public IEnumerable<string> ParameterNames =>
+            DXData.DX21SingleVoiceParametersByName.Keys
+            .Union(DXData.TX81ZAdditionalVoiceDataParametersByName.Keys);
+
+        public object GetParameterValue(string parameterName)
+        {
+            if (DXData.DX21SingleVoiceParametersByName.ContainsKey(parameterName))
+                return DXData.DX21SingleVoiceParametersByName[parameterName].GetValue(
+                    _data, DX21VoiceOffset);
+            if (DXData.TX81ZAdditionalVoiceDataParametersByName.ContainsKey(parameterName))
+                return DXData.TX81ZAdditionalVoiceDataParametersByName[parameterName].GetValue(
+                    _data, TX81ZAdditionalDataOffset);
+            throw new ArgumentException($"Parameter \"{parameterName}\" not found", nameof(parameterName));
+        }
+
+        public void Validate()
+        {
+            DXData.DX21SingleVoiceParameters.ForEach(
+                p => p.Validate(_data, DX21VoiceOffset));
+            DXData.TX81ZAdditionalVoiceDataParameters.ForEach(
+                p => p.Validate(_data, TX81ZAdditionalDataOffset));
+        }
+
+        public Dictionary<string, object> ToDictionary()
+        {
+            Dictionary<string, object> dict = DXData.DX21SingleVoiceParameters.ToDictionary(
+                p => p.Name, p => p.GetValue(_data, DX21VoiceOffset));
+            DXData.TX81ZAdditionalVoiceDataParameters.ForEach(p =>
+                dict.Add(p.Name, p.GetValue(_data, TX81ZAdditionalDataOffset)));
+            return dict;
+        }
+
+        public string ToJSON() => JsonSerializer.Serialize(
+            ToDictionary(), new JsonSerializerOptions { WriteIndented = true });
+
+        #endregion
     }
 }
